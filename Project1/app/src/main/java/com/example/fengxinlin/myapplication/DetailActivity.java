3e27784a2c3047feb79571d85cfb84250db9b91c
gundamkeroro <1056262283fengxl@gmail.com>
2016-11-15 02:14:48 -0500

    Start project_1

---
+++ Project1/app/src/main/java/com/example/fengxinlin/myapplication/DetailActivity.java
@@ -0,0 +1,10 @@
+package com.example.fengxinlin.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by fengxinlin on 11/15/16.
+ */
+public class DetailActivity extends ActionBarActivity{
+
+}
